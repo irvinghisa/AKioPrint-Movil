e32eb7a9c78d973d685d6bb0fe72293b99a19a60
Irving Hinojos Salazar <irving.hi.sa@gmail.com>
2012-08-09 16:21:07 -0600

    Cambi de variables
    Se cambia el titulo de la pantalla principal por el de la aplicacion y
    se elimina variable no utilizada

--- gen/kpmovil/app/R.java
+++ gen/kpmovil/app/R.java
@@ -60,7 +60,6 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
-        public static final int title_activity_iniciar_sesion=0x7f040003;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
